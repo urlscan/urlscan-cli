9c1e7b6a890be89e060c9154ccc03ca6f637ac16
Manabu Niseki <manabu.niseki@gmail.com>
2025-06-07 14:01:34 +0900

    feat: add user

---
+++ cmd/pro/user.go
@@ -0,0 +1,38 @@
+package pro
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	api "github.com/urlscan/urlscan-cli/api"
+	"github.com/urlscan/urlscan-cli/pkg/utils"
+)
+
+var userCmdExample = `  urlscan pro user`
+
+var userCmd = &cobra.Command{
+	Use:     "user",
+	Short:   "Get information about the current user or API key making the request",
+	Example: userCmdExample,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := utils.NewAPIClient()
+		if err != nil {
+			return err
+		}
+
+		url := api.URL("/api/v1/pro/username")
+		result, err := client.Get(url)
+		if err != nil {
+			return err
+		}
+
+		fmt.Print(string(result.Raw))
+
+		return nil
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(userCmd)
+}
