bf80aa9117281a0ebcccc1245f2d57227e00eeca
Manabu Niseki <manabu.niseki@gmail.com>
2025-06-07 09:20:09 +0900

    feat: scan countries

---
+++ cmd/scan/countries.go
@@ -0,0 +1,38 @@
+package scan
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	api "github.com/urlscan/urlscan-cli/api"
+
+	"github.com/urlscan/urlscan-cli/pkg/utils"
+)
+
+var countriesCmdExample = `  urlscan scan countries`
+
+var countriesCmd = &cobra.Command{
+	Use:     "countries",
+	Short:   "Retrieve countries available for scanning using the Scan API",
+	Example: countriesCmdExample,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		client, err := utils.NewAPIClient()
+		if err != nil {
+			return err
+		}
+
+		url := api.URL("/api/v1/availableCountries")
+		result, err := client.Get(url)
+		if err != nil {
+			return err
+		}
+
+		fmt.Print(string(result.Raw))
+
+		return nil
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(countriesCmd)
+}
