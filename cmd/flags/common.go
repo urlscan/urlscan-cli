7b2b8f7d0344b558d5ba47b53b9221a8bb0346f9
Manabu Niseki <manabu.niseki@gmail.com>
2025-07-01 19:19:21 +0900

    Update cmd/flags/common.go
    Co-authored-by: Florian Weingarten <fwgarten@gmail.com>

--- cmd/flags/common.go
+++ cmd/flags/common.go
@@ -3,5 +3,5 @@ package flags
 import "github.com/spf13/cobra"
 
 func AddForceFlag(cmd *cobra.Command) {
-	cmd.Flags().BoolP("force", "f", false, "Enable to force overwriting an existing file.")
+	cmd.Flags().BoolP("force", "f", false, "Force overwrite an existing file.")
 }
