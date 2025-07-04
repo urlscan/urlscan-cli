41fcab6f048502cfbd3e51603b2fd2d867635ee5
Hal Martin <hal@urlscan.io>
2025-07-04 17:27:16 +0200

    Compile regex outside of function, reduce duplicate code
    Signed-off-by: Hal Martin <hal@urlscan.io>

--- pkg/utils/validator.go
+++ pkg/utils/validator.go
@@ -5,13 +5,12 @@ import (
 	"regexp"
 )
 
+var re_ULID = regexp.MustCompile(`^[0-9A-HJKMNPQRSTVWXYZ]{26}$`)
+var re_UUID = regexp.MustCompile(`^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$`)
+
 func ValidateULID(s string) error {
 	// A ULID is a 26-character string using Crockford's Base32 (0-9, A-Z except I, L, O, U)
-	if len(s) != 26 {
-		return fmt.Errorf("invalid ULID format: %s", s)
-	}
-	re := regexp.MustCompile(`^[0-9A-HJKMNPQRSTVWXYZ]{26}$`)
-	if !re.Match([]byte(s)) {
+	if len(s) != 26 || !re_ULID.Match([]byte(s)) {
 		return fmt.Errorf("invalid ULID format: %s", s)
 	}
 	return nil
@@ -19,11 +18,7 @@ func ValidateULID(s string) error {
 
 func ValidateUUID(s string) error {
 	// A UUID is a 36-character string in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-	if len(s) != 36 {
-		return fmt.Errorf("invalid UUID format: %s", s)
-	}
-	re := regexp.MustCompile(`^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$`)
-	if !re.Match([]byte(s)) {
+	if len(s) != 36 || !re_UUID.Match([]byte(s)) {
 		return fmt.Errorf("invalid UUID format: %s", s)
 	}
 	return nil
