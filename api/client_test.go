6531529c2c9042b09242d14eb2da673a9e5fb853
Manabu Niseki <manabu.niseki@gmail.com>
2025-08-08 17:39:55 +0900

    fix: revert wrongly deleted tests

--- api/client_test.go
+++ api/client_test.go
@@ -3,6 +3,7 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"net/url"
 	"testing"
 
@@ -17,6 +18,66 @@ func newTestClient() *Client {
 		Host:   "testserver",
 	})
 	return c
+}
+
+type Counter struct {
+	count int
+}
+
+func (c *Counter) Count() int {
+	original := c.count
+	c.count++
+	return original
+}
+
+func TestRetry(t *testing.T) {
+	defer gock.Off()
+
+	retryCounter := &Counter{}
+
+	gock.New("http://testserver/").
+		Get("/bar").
+		AddMatcher(func(req *http.Request, ereq *gock.Request) (bool, error) { return retryCounter.Count() == 0, nil }).
+		Reply(http.StatusTooManyRequests).
+		SetHeaders(map[string]string{"X-Rate-Limit-Reset-After": "0"}).
+		JSON(map[string]string{"foo": "bar"})
+
+	gock.New("http://testserver/").
+		Get("/bar").
+		AddMatcher(func(req *http.Request, ereq *gock.Request) (bool, error) { return retryCounter.Count() == 1, nil }).
+		Reply(http.StatusOK).
+		JSON(map[string]string{"foo": "bar"})
+
+	c := newTestClient()
+	got, err := c.NewRequest().Get("/bar")
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"foo\":\"bar\"}\n", string(got.body))
+	assert.Equal(t, gock.IsDone(), true)
+}
+
+func TestWaitAndGetResult(t *testing.T) {
+	defer gock.Off()
+
+	gock.New("http://testserver/").
+		Post("/api/v1/scan/").
+		Reply(http.StatusOK).
+		JSON(map[string]string{"uuid": "dummy"})
+
+	gock.New("http://testserver/").
+		Get("/api/v1/result/dummy/").
+		Reply(http.StatusOK).
+		JSON(map[string]string{"foo": "bar"})
+
+	c := newTestClient()
+	// do scan to get UUID
+	scanRes, err := c.Scan("http://localhost")
+	assert.NoError(t, err)
+
+	// wait for the result
+	got, err := c.WaitAndGetResult(t.Context(), scanRes.UUID, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"foo\":\"bar\"}\n", string(got.body))
+	assert.Equal(t, gock.IsDone(), true)
 }
 
 func TestGet(t *testing.T) {
