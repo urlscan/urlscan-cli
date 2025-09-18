d9822f6e48a52852ad222f98e96b42413afb2810
Manabu Niseki <manabu.niseki@gmail.com>
2025-09-18 16:09:31 +0900

    feat: allow bulk-submitting IP and domain inputs

--- pkg/utils/validator.go
+++ pkg/utils/validator.go
@@ -2,13 +2,18 @@ package utils
 
 import (
 	"fmt"
+	"net"
 	"net/url"
 	"regexp"
+	"slices"
+	"strings"
 )
 
 var (
-	re_ULID = regexp.MustCompile(`^[0-9A-HJKMNPQRSTVWXYZ]{26}$`)
-	re_UUID = regexp.MustCompile(`^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$`)
+	re_ULID       = regexp.MustCompile(`^[0-9A-HJKMNPQRSTVWXYZ]{26}$`)
+	re_UUID       = regexp.MustCompile(`^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$`)
+	re_dot        = regexp.MustCompile(`\.`)
+	re_digit_only = regexp.MustCompile(`^\d+$`)
 )
 
 func ValidateULID(s string) error {
@@ -40,5 +45,74 @@ func ValidateURL(s string) error {
 	if parsed.Host == "" {
 		return fmt.Errorf("invalid URL format: %s", s)
 	}
+
+	// host should be a valid domain or an IP address
+	if ValidateDomain(parsed.Host) != nil && ValidateIP(parsed.Host) != nil {
+		return fmt.Errorf("invalid URL format: %s", s)
+	}
+
 	return nil
+}
+
+func ValidateDomain(s string) error {
+	// check whether it starts with https:// or http://
+	if strings.HasPrefix(s, "http://") || strings.HasPrefix(s, "https://") {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+
+	parsed, err := url.Parse("http://" + s)
+	if err != nil {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+
+	if parsed.Host == "" {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+
+	parts := re_dot.Split(parsed.Host, -1)
+	// should have one or more dots
+	if len(parts) <= 1 {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+	// ensure that there is no empty part in the domain (e.g. example..com)
+	if slices.Contains(parts, "") {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+
+	// should not all the parts are digits to avoid IP addresses like string (e.g. 192.168.1)
+	allDigits := true
+	for _, part := range parts {
+		if !re_digit_only.MatchString(part) {
+			allDigits = false
+			break
+		}
+	}
+	if allDigits {
+		return fmt.Errorf("invalid domain format: %s", s)
+	}
+
+	return nil
+}
+
+func ValidateIP(s string) error {
+	if net.ParseIP(s) == nil {
+		return fmt.Errorf("invalid IP format: %s", s)
+	}
+	return nil
+}
+
+func ValidateNetworkIndicator(s string) error {
+	if ValidateIP(s) == nil {
+		return nil
+	}
+
+	if ValidateDomain(s) == nil {
+		return nil
+	}
+
+	if ValidateURL(s) == nil {
+		return nil
+	}
+
+	return fmt.Errorf("invalid network indicator format: %s", s)
 }
