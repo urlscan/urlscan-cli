bdd1c920c30a3c4cab034322ef9a4cc4c0976b4c
Manabu Niseki <manabu.niseki@gmail.com>
2026-01-08 13:56:26 +0900

    refactor: use path directly in the list cmd

--- cmd/pro/datadump/list.go
+++ cmd/pro/datadump/list.go
@@ -9,34 +9,32 @@ import (
 	"github.com/urlscan/urlscan-cli/pkg/utils"
 )
 
-var ListCmdExample = `  urlscan pro datadump list --time-window days --file-type api --date 20260101`
+var ListCmdExample = `  urlscan pro datadump list days/api
+  urlscan pro datadump list hours/api/20260101
+  echo "<path>" | urlscan pro datadump list -
+
+  NOTE: path format is <time-window>/<file-type>/<date>
+        - time-window: days | hours | minutes
+        - file-type: api | search | screenshot | dom
+        - date: YYYYMMDD format date (optional if time-window is days)`
 
 var listCmd = &cobra.Command{
 	Use:     "list",
 	Short:   "Get the list of data dump files",
 	Example: ListCmdExample,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		reader := utils.StringReaderFromCmdArgs(args)
+		path, err := reader.ReadString()
+		if err != nil {
+			return err
+		}
+
 		client, err := utils.NewAPIClient()
 		if err != nil {
 			return err
 		}
 
-		timeWindow, err := cmd.Flags().GetString("time-window")
-		if err != nil {
-			return err
-		}
-
-		fileType, err := cmd.Flags().GetString("file-type")
-		if err != nil {
-			return err
-		}
-
-		date, err := cmd.Flags().GetString("date")
-		if err != nil {
-			return err
-		}
-
-		resp, err := client.NewRequest().Get(api.PrefixedPath(fmt.Sprintf("/datadump/list/%s/%s/%s", timeWindow, fileType, date)))
+		resp, err := client.NewRequest().Get(api.PrefixedPath(fmt.Sprintf("/datadump/list/%s", path)))
 		if err != nil {
 			return err
 		}
@@ -49,8 +47,4 @@ var listCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(listCmd)
-
-	listCmd.Flags().StringP("time-window", "t", "", "Time window for the data dump (days, hours, minutes)")
-	listCmd.Flags().StringP("file-type", "f", "", "File type for the data dump (api, search, screenshot, dom)")
-	listCmd.Flags().StringP("date", "d", "", "Date for the data dump (e.g., 20260101)")
 }
--- docs/urlscan_pro_datadump_list.md
+++ docs/urlscan_pro_datadump_list.md
@@ -9,16 +9,20 @@ urlscan pro datadump list [flags]
 ### Examples
 
 ```
-  urlscan pro datadump list --time-window days --file-type api --date 20260101
+  urlscan pro datadump list days/api
+  urlscan pro datadump list hours/api/20260101
+  echo "<path>" | urlscan pro datadump list -
+
+  NOTE: path format is <time-window>/<file-type>/<date>
+        - time-window: days | hours | minutes
+        - file-type: api | search | screenshot | dom
+        - date: YYYYMMDD format date (optional if time-window is days)
 ```
 
 ### Options
 
 ```
-  -d, --date string          Date for the data dump (e.g., 20260101)
-  -f, --file-type string     File type for the data dump (api, search, screenshot, dom)
-  -h, --help                 help for list
-  -t, --time-window string   Time window for the data dump (days, hours, minutes)
+  -h, --help   help for list
 ```
 
 ### SEE ALSO
