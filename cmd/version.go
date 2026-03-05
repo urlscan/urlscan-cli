9f01b1c657c272c9f9ad80e77313ca6780705738
Manabu Niseki <manabu.niseki@gmail.com>
2026-03-05 19:11:35 +0900

    feat: show update notice (#117)
    * feat: show update notice
    
    * refactor: remove notice from root
    
    * refactor: use a channel with a short timeout & silence the error

--- cmd/version.go
+++ cmd/version.go
@@ -2,10 +2,35 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
+
 	"github.com/urlscan/urlscan-cli/pkg/version"
 )
+
+const (
+	orange = "\033[38;5;208m"
+	reset  = "\033[0m"
+)
+
+func printUpdateNotice(latest string) {
+	msg := fmt.Sprintf("Update available: %s", latest)
+
+	width, _, err := term.GetSize(int(os.Stderr.Fd()))
+	if err != nil || width <= 0 {
+		width = 80
+	}
+
+	if pad := width - len(msg); pad > 0 {
+		fmt.Fprintf(os.Stderr, "%s%s%s%s\n", orange, strings.Repeat(" ", pad), msg, reset)
+	} else {
+		fmt.Fprintf(os.Stderr, "%s%s%s\n", orange, msg, reset)
+	}
+}
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -17,6 +42,35 @@ var versionCmd = &cobra.Command{
 
 		fmt.Printf("urlscan-cli %s\n", version.Version)
 		return nil
+	},
+	PersistentPostRunE: func(cmd *cobra.Command, args []string) (err error) {
+		type result struct {
+			version   string
+			hasUpdate bool
+		}
+		ch := make(chan result, 1)
+
+		timeout := 5
+
+		go func() {
+			latest, err := version.CheckLatest(timeout)
+			hasUpdate := false
+			if err == nil {
+				hasUpdate = version.IsNewer(version.Version, latest)
+			}
+			ch <- result{latest, hasUpdate}
+		}()
+
+		select {
+		case r := <-ch:
+			if r.hasUpdate {
+				printUpdateNotice(r.version)
+			}
+		case <-time.After(time.Duration(timeout) * time.Second): // don't hang if GitHub is slow/unreachable
+			// silently skip
+		}
+
+		return err
 	},
 }
 
---
+++ pkg/version/update.go
@@ -0,0 +1,61 @@
+package version
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/Masterminds/semver/v3"
+)
+
+const (
+	repoOwner = "urlscan"
+	repoName  = "urlscan-cli"
+)
+
+type githubRelease struct {
+	TagName string `json:"tag_name"`
+}
+
+func CheckLatest(timeout int) (string, error) {
+	client := &http.Client{Timeout: time.Duration(timeout) * time.Second}
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases/latest", repoOwner, repoName)
+
+	resp, err := client.Get(url)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		return "", nil
+	}
+	defer func() {
+		closeErr := resp.Body.Close()
+		if err == nil {
+			err = closeErr
+		}
+	}()
+
+	var release githubRelease
+	err = json.NewDecoder(resp.Body).Decode(&release)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimPrefix(release.TagName, "v"), err
+}
+
+func IsNewer(current, latest string) bool {
+	// for dev version (= an empty version), always show update notice for testing purpose
+	if current == "" {
+		return true
+	}
+
+	a, err := semver.NewVersion(current)
+	if err != nil {
+		return false
+	}
+	b, err := semver.NewVersion(latest)
+	if err != nil {
+		return false
+	}
+	return a.Compare(b) == -1
+}
